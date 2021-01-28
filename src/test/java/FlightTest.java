808edf9fc2c61abc3df58c15b8d9de6b3bafcbdf
Jack Portwood <Jack.Portwood14@gmail.com>
2021-01-28 12:36:12 +0000

    Test changed to fail

--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -1,2 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class FlightTest {
+
+    private Passenger pass1;
+    private Passenger pass2;
+    private Flight flight;
+
+    @Before
+    public void before(){
+        pass1=new Passenger("Joseph",1,"JS300");
+        pass2=new Passenger("Jack",2,"JP200");
+        flight=new Flight(Plane.AIRBUSA830, "JS300","Edinburgh","Glasgow","13:00");
+
+    }
+
+    @Test
+    public void canGetPlane(){
+        assertEquals(Plane.AIRBUSA830, flight.getPlane());
+    }
+
+
+
+
+
 }
